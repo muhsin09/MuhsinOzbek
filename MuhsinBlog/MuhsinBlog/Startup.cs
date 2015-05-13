000e05a647ca8edc1dcb3f3eda7fdcd94a430f20
muhsin09 <muhsinozbek09@gmail.com>
2015-05-13 16:50:47 +0300

    rest

---
+++ MuhsinBlog/MuhsinBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MuhsinBlog.Startup))]
+namespace MuhsinBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
